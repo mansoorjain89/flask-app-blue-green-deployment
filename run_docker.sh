c0895aa399651cb93bed5caab3f89fcc9dfca051
Manjunath Ravi <mravi@us.ibm.com>
2020-06-14 18:41:28 -0500

    Authorize ECR to push the image

--- run_docker.sh
+++ run_docker.sh
@@ -5,3 +5,5 @@ docker build -t flask-app-ecr .
 docker tag flask-app-ecr:latest 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v1
 
 docker image ls
+
+docker push 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v1
