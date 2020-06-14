814316cc314910ae5489ec475d37e30973685fdc
Manjunath Ravi <mravi@us.ibm.com>
2020-06-14 18:26:06 -0500

    Include aws tagging on images

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t mansoorjain89/flask-app-deployment:v2 .
+docker build -t flask-app-ecr .
+
+docker tag flask-app-ecr:latest 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v1
 
 docker image ls
