bca84542ab7c0218c47049e3cdfda2b5c1902f27
Manjunath Ravi <mravi@us.ibm.com>
2020-06-19 14:48:51 -0500

    Implement Blue/Green Deployment strategy

--- run_docker.sh
+++ run_docker.sh
@@ -2,10 +2,10 @@
 
 docker build -t flask-app-ecr .
 
-docker tag flask-app-ecr:latest 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v1
+docker tag flask-app-ecr:latest 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v2
 
 docker image ls
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 905005439868.dkr.ecr.us-east-2.amazonaws.com
 
-docker push 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v1
+docker push 905005439868.dkr.ecr.us-east-2.amazonaws.com/flask-app-ecr:v2
