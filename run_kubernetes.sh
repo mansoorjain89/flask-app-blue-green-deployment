7d97eab73bb47e95d208db1d2827c48aff18fda3
Manjunath Ravi <mravi@us.ibm.com>
2020-06-19 19:20:42 -0500

    Blue deployment for app

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,9 +2,9 @@
 
 aws eks --region us-east-2 update-kubeconfig --name udacity_capstone
 
-kubectl apply -f ./kube_deployment_green.yaml
+kubectl apply -f ./kube_deployment_blue.yaml
 
-kubectl expose deployment flask-app-green --type=LoadBalancer --name=flask-app-green
+kubectl expose deployment flask-app-blue --type=LoadBalancer --name=flask-app-blue
 
 sleep 15
 
